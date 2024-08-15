ca0a0c51c498707ae8c4749bb6e92895a312278f
thuso kharibe <thusok@runninghill.co.za>
2024-08-15 15:36:00 +0200

    Adding products to products table

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Entity_Framework_Core_Fundies.Models;
+using Entity_Framework_Core_Fundies.Data;
+using System.Diagnostics;
+
+using PizzaDbContext context = new PizzaDbContext();
+
+Product veggieSpecial = new Product()
+{
+    Name = "Veggie Special Pizza",
+    Price = 9.99M
+};
+context.Products.Add(veggieSpecial);
+
+Product deluxeMeat = new Product()
+{
+    Name = "Deluxe Meat Pizza",
+    Price = 12.99M
+};
+context.Add(deluxeMeat);
+
+context.SaveChanges();
+
