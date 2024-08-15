c620f707283e06408c31f9ebc2a3de7f0313d9b5
thuso kharibe <thusok@runninghill.co.za>
2024-08-15 15:55:37 +0200

    Reading from db using linq queries as well as fluent api

--- Program.cs
+++ Program.cs
@@ -4,19 +4,46 @@ using System.Diagnostics;
 
 using PizzaDbContext context = new PizzaDbContext();
 
-Product veggieSpecial = new Product()
-{
-    Name = "Veggie Special Pizza",
-    Price = 9.99M
-};
-context.Products.Add(veggieSpecial);
+var fluentProducts = context.Products
+    .Where(p => p.Price > 10.00M)
+    .OrderBy(p => p.Name);
 
-Product deluxeMeat = new Product()
+Console.WriteLine("Fluent API Query");
+Console.WriteLine(new string('-', 20));
+
+foreach (Product product in fluentProducts)
 {
-    Name = "Deluxe Meat Pizza",
-    Price = 12.99M
-};
-context.Add(deluxeMeat);
+    Console.WriteLine($"Id: {product.Id}");
+    Console.WriteLine($"Name: {product.Name}");
+    Console.WriteLine($"Price: {product.Price}");
+    Console.WriteLine(new string('-', 20));
+}
+
+
+var veggieSpecial = context.Products
+    .Where(p => p.Name == "Veggie Special Pizza")
+    .FirstOrDefault();
+
+if ( veggieSpecial is Product )
+{
+    context.Remove( veggieSpecial );
+}
 
 context.SaveChanges();
+
+var linkProducts = from product in context.Products
+                   where product.Price > 10.00M
+                   orderby product.Name
+                   select product;
+
+Console.WriteLine("Link Query");
+Console.WriteLine(new string('-', 20));
+
+foreach (Product product in linkProducts)
+{
+    Console.WriteLine($"Id: {product.Id}");
+    Console.WriteLine($"Name: {product.Name}");
+    Console.WriteLine($"Price: {product.Price}");
+    Console.WriteLine(new string('-', 20));
+}
 
